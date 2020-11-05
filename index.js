d0da6cab2ac97fba19030b9af2a9c279963373c1
jefersonpotosi3 <jepore97@gmail.com>
2020-11-05 08:24:21 -0500

    make it better

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app= require('./app/app')
 require('./app/config/conection')
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(app.get('port'),(error)=>{
+app.listen(process.env.PORT, '0.0.0.0',(error)=>{
     if(error){
         console.log(`ha ocurrido un error: ${error}`)
     } else{
