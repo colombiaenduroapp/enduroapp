d95a8b9971d4a67122e4ea15e16cc412a960d743
jefersonpotosi3 <jepore97@gmail.com>
2020-11-07 16:26:14 -0500

    Commit message

--- app/config/keys.js
+++ app/config/keys.js
@@ -2,9 +2,9 @@ module.exports = {
 
     database: {
         host: '198.71.225.61',
-        user: 'jepore',
-        password: '8O!gsk05',
-        database: 'enduroapp',
+        user: 'jeferson',
+        password: 'enduroApp3',
+        database: 'enduro_db',
         port: 3306
    }
 };
