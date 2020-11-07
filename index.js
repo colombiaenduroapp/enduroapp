6097117c6aea115638cb7937eb8310860d25efdc
jefersonpotosi3 <jepore97@gmail.com>
2020-11-07 17:06:51 -0500

    Commit message

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
 const app= require('./app/app')
 require('./app/config/conection')
-const port = process.env.port || 5000
 
-app.listen(port, '0.0.0.0',(error)=>{
+app.listen(process.env.PORT || 5000, '0.0.0.0',(error)=>{
     if(error){
         console.log(`ha ocurrido un error: ${error}`)
     } else{
