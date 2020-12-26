caa876e25ef11a31a09d9d22989c619cc6f6f14a
jepore97 <jepore97@gmail.com>
2020-12-25 21:34:16 -0500

    Elimino node modules

--- app/routes/ciudad.route.js
+++ app/routes/ciudad.route.js
@@ -3,11 +3,11 @@ const route = require('express').Router();
 
 const ciudad= require('../controllers/ciudad.controller')
 
-route.route('/ciudad').get(ciudad.getCiudad)
+route.route('/').get(ciudad.getCiudad)
 // route.route('/ciudad:cd_cdgo').get(getOneCiudad)
 
 // route.route('/ciudad/image').post(ciudad.addImage)
-route.route('/ciudad').post(ciudad.addCiudad)
+route.route('/').post(ciudad.addCiudad)
 // route.route('/ciudad:cd_cdgo').put(editCiudad)
 // route.route('/ciudad:cd_cdgo').delete(deleteCiudad)
 
--- app/routes/index.route.js
+++ app/routes/index.route.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 
 router.use('/sede', require('./sede.route'));
-router.use(require('./ciudad.route'));
+router.use('ciudad',require('./ciudad.route'));
 router.use(require('./evento.route'));
 router.use(require('./empresa.route'));
 
